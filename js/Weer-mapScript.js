effc5f5a5b6b93bde58ec9b542453ff848a85533
SmoldersKasper <kasper.smolders@gmail.com>
2024-05-09 23:35:33 +0200

    scroll fix + map layers

--- js/Weer-mapScript.js
+++ js/Weer-mapScript.js
@@ -5,3 +5,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+map.addLayer(new L.TileLayer("https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=6ddd690925f3528383e8464ae5ab3b7e"));
+
+map.addLayer(new L.TileLayer("https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=6ddd690925f3528383e8464ae5ab3b7e"));
